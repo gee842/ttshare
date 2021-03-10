4a2ba9972e1e3a268f28ec291935743af876c42e
Ethan Ooi <ethanooiyien@gmail.com>
2021-03-10 17:09:17 +0800

    e

--- start.py
+++ start.py
@@ -2,6 +2,6 @@ from app.flaskapp import app
 
 if __name__ == "__main__":
     app.config['JSON_SORT_KEYS'] = False
-    app.run(debug=True, port=8000)
+    app.run(host='0.0.0.0', port=8001)
 
 #5103618
